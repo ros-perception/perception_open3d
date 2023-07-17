eff97534865f95e7b2f5ae778296f7fb1e56c7e3
tiesus <t.junge@silolytics.de>
2023-07-17 19:22:58 +0200

    Add bindings for inline functions

--- open3d_conversions/src/python/open3d_conversions_pybind.cpp
+++ open3d_conversions/src/python/open3d_conversions_pybind.cpp
@@ -1,11 +1,15 @@
-#include <pybind11/numpy.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/stl_bind.h>
-
+#include <pybind11/pybind11.h> 
+#include <pybind11/detail/internals.h>
+#include <pybind11/eigen.h>
 #include <pybind11/functional.h>
+#include <pybind11/chrono.h>
 #include <pybind11/numpy.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <Eigen/Core>
 #include "open3d_conversions/open3d_conversions.h"
+
+PYBIND11_MAKE_OPAQUE(std::vector<Eigen::Vector3d>);
 
 PYBIND11_MODULE(open3d_conversions_py, m) {
     namespace py = pybind11;
@@ -38,8 +42,17 @@ PYBIND11_MODULE(open3d_conversions_py, m) {
               open3d_conversions::rosToOpen3d(ros_pc2, o3d_pc, skip_colors);
           },
           py::arg("ros_pc2"), py::arg("o3d_pc"), py::arg("skip_colors") = false);
+    
+    m.def("addPointField",
+          [](sensor_msgs::PointCloud2& cloud_msg, const std::string& name,
+             int count, int datatype, int offset) {
+              addPointField(cloud_msg, name, count, datatype, offset);
+          },
+          py::arg("cloud_msg"), py::arg("name"), py::arg("count"),py::arg("datatype"), py::arg("offset"));
+
+     m.def("sizeOfPointField",
+          [](int datatype) {
+              sizeOfPointField(datatype);
+          },
+          py::arg("datatype"));
 }
-
-
-
-
